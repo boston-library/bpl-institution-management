6a16bdb47360ed27125cd4d37e387255bc20a7a6
Steven Anderson <sanderson@bpl.org>
2016-06-24 17:05:36 -0400

    Allow slashes in institution names

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -5,7 +5,7 @@ class Institution < ActiveRecord::Base
 
   validates :name,
     uniqueness: true,
-    format: { with: /\A[a-zA-Z0-9._ \-'&,:]+\z/,
+    format: { with: /\A[a-zA-Z0-9._ \-'&,:\/]+\z/,
       :message => "Only letters, numbers, hyphens, underscores, spaces, apostrophes, ampersand, commas, colons, and periods are allowed"}
 
 end
