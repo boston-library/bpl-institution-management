f964638784d2335588ce32dd4a97f5ad88cff0af
Steven Anderson <sanderson@bpl.org>
2016-06-24 16:26:30 -0400

    Fix the populate all path

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Bpl::InstitutionManagement::Engine.routes.draw do
       resources :institutions do
         resources :users, :only=>[:create, :destroy], :controller => "bpl/institutions_management/user_institutions"
       end
+      match 'populate_all', :to => 'institutions#populate_all', :as => 'populate_all', via: [:get]
     end
   end
-
-  get 'populate_all', :to => 'bpl/institutions_management/institutions#populate_all', :as => 'populate_all'
 end
 
