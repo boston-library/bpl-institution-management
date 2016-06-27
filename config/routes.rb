e35fa066bad37da73ea545adc113e084ab546d6e
Steven Anderson <sanderson@bpl.org>
2016-06-27 16:45:45 -0400

    Fix a route for user permissions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Bpl::InstitutionManagement::Engine.routes.draw do
   scope module: 'bpl' do
     scope module: 'institution_management' do
       resources :institutions do
-        resources :users, :only=>[:create, :destroy], :controller => "bpl/institutions_management/user_institutions"
+        resources :users, :only=>[:create, :destroy], :controller => "user_institutions"
       end
       match 'populate_all', :to => 'institutions#populate_all', :as => 'populate_all', via: [:get]
     end
